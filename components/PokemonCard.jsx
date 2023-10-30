83f55ebab9a13eccb8e40eca5bf480629e0e240c
Thibault R <44661431+R-Thibault@users.noreply.github.com>
2023-10-30 21:18:37 +0100

    React Basics 04 - Pokédex - Passer des props

--- components/PokemonCard.jsx
+++ components/PokemonCard.jsx
@@ -1,16 +1,6 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+function PokemonCard(pokemon) {
+  console.log(pokemon);
 
-function PokemonCard() {
-  let pokemon = pokemonList[0];
   return (
     <figure>
       {pokemon.imgSrc ? <img src={pokemon.imgSrc}></img> : <p>???</p>}
